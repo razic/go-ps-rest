2475f0651257c60a490d14d59d8605258090b820
Zachary Adam Kaplan <razic@viralkitty.com>
2017-07-04 20:21:40 +0000

    implement main.go

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/spf13/afero"
+)
 
 func main() {
-	http.HandleFunc("/ps", HandleHTTP)
+	// initialize the process list
+	processList := &ProcessList{fs: afero.NewOsFs()}
+
+	// initialize the http handler
+	httpHandler := &HTTPHandler{processLister: processList}
+
+	// register the http handler for the /ps endpoint
+	http.Handle("/ps", httpHandler)
+
+	// start the http server
 	http.ListenAndServe(":8080", nil)
 }
