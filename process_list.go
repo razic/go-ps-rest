25c245b573267a62d8747178d3e5c155fc2f1f64
Zachary Adam Kaplan <razic@viralkitty.com>
2017-07-05 03:35:45 +0000

    add GetProcDirs to ProcessList

--- process_list.go
+++ process_list.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/spf13/afero"
+import (
+	"os"
+	"regexp"
+
+	"github.com/spf13/afero"
+)
 
 // ProcessLister implements the ProcessLister interface
 type ProcessLister interface {
@@ -15,4 +20,26 @@ type ProcessList struct {
 // List returns process list
 func (l *ProcessList) List() []*Process {
 	return []*Process{}
+}
+
+// GetProcDirs returns a list of proc dirs
+func (l *ProcessList) GetProcDirs() ([]os.FileInfo, error) {
+	// get list of all directories in /proc
+	dirsA, err := afero.ReadDir(l.fs, "/proc")
+
+	if err != nil {
+		return nil, err
+	}
+
+	// create a new slice, using same backing array
+	dirsB := dirsA[:0]
+
+	// filter out the directories not matching a numeric pattern
+	for _, dir := range dirsA {
+		if ok, _ := regexp.Match(`^\d+$`, []byte(dir.Name())); ok {
+			dirsB = append(dirsB, dir)
+		}
+	}
+
+	return dirsB, nil
 }
--- process_list_test.go
+++ process_list_test.go
@@ -1,6 +1,39 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestProccessListList(t *testing.T) {
+	"github.com/spf13/afero"
+)
+
+func TestProccessListGetProcDirs(t *testing.T) {
+	// initialize an in-memory file system
+	fs := afero.NewMemMapFs()
+
+	// initialize the process list with the in memory file system
+	processList := &ProcessList{fs: fs}
+
+	// populate the in-memory file system with /proc
+	fs.MkdirAll("/sock/1", 0755)
+	fs.MkdirAll("/proc/1", 0755)
+	fs.MkdirAll("/proc/2", 0755)
+	fs.MkdirAll("/proc/3", 0755)
+	fs.MkdirAll("/proc/a", 0755)
+	fs.MkdirAll("/proc/b", 0755)
+	fs.MkdirAll("/proc/c", 0755)
+
+	dirs, err := processList.GetProcDirs()
+	occs := make(map[string]int)
+
+	if err != nil {
+		t.Fatal("unexpected error")
+	}
+
+	for _, dir := range dirs {
+		occs[dir.Name()]++
+	}
+
+	if len(occs) != 3 {
+		t.Fatalf("expected only 3 dirs, got %d", len(occs))
+	}
 }
